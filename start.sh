aa09126a51e2fb64e5957fd21f47dcb06f8fbd59
Shubhi1352 <103235395+Shubhi1352@users.noreply.github.com>
2026-04-23 02:46:30 +0530

    fix: passing HF env vars as nakama runtime env

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 /nakama/nakama migrate up --database.address $DATABASE_URL
-/nakama/nakama --config /nakama/data/local.yml --database.address $DATABASE_URL
+/nakama/nakama --config /nakama/data/local.yml \
+  --database.address $DATABASE_URL \
+  --runtime.env "HF_TOKEN=$HF_TOKEN" \
+  --runtime.env "HF_MODEL=$HF_MODEL"
